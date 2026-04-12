4c8a2b98eeb64a356fb535dca9fbbccaa5af6ccc
= <=>
2026-04-12 09:19:25 +0500

    code updated

--- controllers/OrderControllers.js
+++ controllers/OrderControllers.js
@@ -1,4 +1,6 @@
+import { Error } from "mongoose";
 import Order from "../models/orderModel.js";
+import User from "../models/userModel.js";
 
 export const createOrder = async (req,res) => {
     try {
@@ -30,6 +32,79 @@ export const createOrder = async (req,res) => {
         })
     } catch (error) {
         return res.status(500).json({
+            success : false,
+            error
+        })
+    }
+}
+
+// get single order api
+
+export const getSingleOrder = async () => {
+    try {
+        const order = await Order.findById(req.params.id).populate("user", "name email")
+        if(!order){
+            return res.status(400).json({
+                success : false,
+                message : "Order not found"
+            })
+        }
+
+        return res.status(200).json({
+            sucess : true,
+            order
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success : false,
+            error
+        })
+    }
+}
+
+export const myOrderDetails = async (req,res) => {
+    try {
+        const order = await User.find({user : req.user.id});
+        if(!order){
+            return res.status(400).json({
+                success : false,
+                message : "Order not found"
+            })
+        }
+        return res.status(200).json({
+            success : true,
+            order
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success : false,
+            error
+        })
+    }
+}
+
+export const getAllOrders = async (req,res) => {
+    try {
+        const orders = await Order.find();
+        if(!orders){
+            return res.status(400).json({
+                success : false,
+                message : "orders not found"
+            })
+        }
+
+        let total = 0;
+        orders.forEach((order)=>{
+            total = total + order.totalPrice
+        })
+
+        return res.status(200).json({
+            success : true,
+            orders,
+            total
+        })
+    } catch (error) {
+        return res.status(200).json({
             success : false,
             error
         })
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,7 +1,10 @@
 import express from "express";
-import { isAuthenticatedUser } from "../util/userAuth.js";
-import { createOrder } from "../controllers/OrderControllers.js";
+import { isAdmin, isAuthenticatedUser } from "../util/userAuth.js";
+import { createOrder, getAllOrders, getSingleOrder, myOrderDetails } from "../controllers/OrderControllers.js";
 const orderRouter = express.Router()
 
 orderRouter.post("/create-order", isAuthenticatedUser, createOrder);
+orderRouter.get("/order-details/:id", isAuthenticatedUser, isAdmin("admin"), getSingleOrder);
+orderRouter.get("/my-order", isAuthenticatedUser, myOrderDetails);
+orderRouter.get("/all-orders", isAuthenticatedUser, isAdmin("admin"), getAllOrders)
 export default orderRouter;
